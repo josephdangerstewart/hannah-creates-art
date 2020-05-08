71541cae087739f763214999b6eaca627df3780e
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-05-07 20:03:46 -0700

    Make photo gallery responsive

--- components/image-gallery/styled.ts
+++ components/image-gallery/styled.ts
@@ -4,11 +4,22 @@ export const Container = styled.div`
 	display: flex;
 	flex-wrap: wrap;
 	margin: 0 60px;
+
+	@media ${({ theme }) => theme.breakpoints.mobile} {
+		flex-direction: column;
+		margin: auto;
+	}
 `;
 
 export const ImageWrapper = styled.div`
 	flex: 0 0 50%;
 	padding: 18px;
+
+	@media ${({ theme }) => theme.breakpoints.mobile} {
+		width: 100%;
+		margin: auto;
+		flex: 0 0 100%;
+	}
 `;
 
 export const Image = styled.img`
