bdeeaa5c73923aaa73d283c5ee920c048ed7ee30
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-05-07 20:00:43 -0700

    Make footer responsive

--- components/footer/styled.ts
+++ components/footer/styled.ts
@@ -6,30 +6,35 @@ export const Container = styled.div`
 	display: flex;
 	flex-wrap: wrap;
 	justify-content: space-between;
+
+	@media ${({ theme }) => theme.breakpoints.mobile} {
+		flex-direction: column-reverse;
+		align-items: center;
+	}
 `;
 
 export const LogoCircle = styled.div<{ color: keyof Theme['color' ] }>`
 	width: 36px;
 	height: 36px;
 	border-radius: 50%;
-	margin-right: 8px;
 	background-color: ${({ color, theme }) => theme.color[color]};
+	margin-right: 8px;
+	&:last-child {
+		margin-right: 0;
+	}
 `;
 
 export const LogoCircleContainer = styled.div`
 	display: flex;
 `;
 
-export const ContactLinksContainer = styled.div`
-	display: flex;
-
-	& svg {
-		margin-left: 8px;
-	}
-`;
-
 export const ContactLink = styled.a<{ color: keyof Theme['color'] }>`
 	cursor: pointer;
+	margin-right: 8px;
+
+	&:last-child {
+		margin-right: 0;
+	}
 
 	& svg path {
 		fill: ${({ color, theme }) => theme.color[color]};
@@ -38,5 +43,13 @@ export const ContactLink = styled.a<{ color: keyof Theme['color'] }>`
 
 	&:hover svg path {
 		fill: ${({ color, theme }) => theme.hover(theme.color[color])};
+	}
+`;
+
+export const ContactLinksContainer = styled.div`
+	display: flex;
+
+	@media ${({ theme }) => theme.breakpoints.mobile} {
+		margin-bottom: 8px;
 	}
 `;
