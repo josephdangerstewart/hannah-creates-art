1350529f4ae39bc7be56c3b2867418f72ab4c3a9
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-05-08 22:49:14 -0700

    Add link to resume

--- pages/about.tsx
+++ pages/about.tsx
@@ -20,7 +20,7 @@ export default function About() {
 		<Page>
 			<ContentHeader
 				title="Hi I'm Hannah!"
-				description="I am a visual artist based in Los Angeles, recent graduate from Biola University for a BFA in Design. From being interested in illustrating my own stories and creatures, to falling in love with visual design and language, I create art that strives to relate to other people. I specialize in art and spirituality, studying how the two are congruent with each other in order to better understand the humans we are designed to be."
+				description={'I am a visual artist based in Los Angeles, recent graduate from Biola University for a BFA in Design. From being interested in illustrating my own stories and creatures, to falling in love with visual design and language, I create art that strives to relate to other people. I specialize in art and spirituality, studying how the two are congruent with each other in order to better understand the humans we are designed to be.\n\n[Resume](https://res.cloudinary.com/josephdangerstewart/image/upload/v1589003199/hannah-creates-art/hannahresume.pdf)'}
 				color="accent1"
 				imageUrl="https://res.cloudinary.com/josephdangerstewart/image/upload/v1588995401/hannah-creates-art/Profile_Picture.jpg"
 				fillImage
@@ -32,6 +32,7 @@ export default function About() {
 						<A href="tel:1-949-395-4868" target="_blank" rel="noopener noreferrer nofollow">949.395.4868</A>
 						<br/>
 						<A href="mailto:hannah.mclaughlin.art@gmail.com" target="_blank" rel="noopener noreferrer nofollow">hannah.mclaughlin.art@gmail.com</A>
+						<br/>
 					</p>
 				</BodyText>
 			</Container>
