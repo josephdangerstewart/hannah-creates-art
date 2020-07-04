955382287e8d8cc74417b9e0c29c64448e2dda69
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-07-04 00:23:08 -0700

    Fix about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Page } from '../components/page';
 import { ContentHeader } from '../components/content-header';
-import { BodyText, A } from '../components/common-styled';
-import { styled } from '../components/theme';
 
-const Container = styled.div`
-	max-width: 800px;
-	padding: 0 60px;
-	margin: auto;
-	word-break: break-all;
+const aboutContent = `
+I am a visual artist based in Los Angeles, recent graduate from Biola University for a BFA in Design.
+From being interested in illustrating my own stories and creatures, to falling in love with visual design
+and language, I create art that strives to relate to other people. I specialize in art and spirituality,
+studying how the two are congruent with each other in order to better understand the humans we are designed
+to be.
 
-	@media ${({ theme }) => theme.breakpoints.mobile} {
-		padding: 0 15px;
-	}
+[Resume](https://res.cloudinary.com/josephdangerstewart/image/upload/v1589003199/hannah-creates-art/hannahresume.pdf)
+
+**CONTACT INFO**
+
+[1-949-395-4868](tel:1-949-395-4868)
+
+<hannah.r.mclaughlin.art@gmail.com>
 `;
 
 export default function About() {
@@ -20,22 +23,11 @@ export default function About() {
 		<Page>
 			<ContentHeader
 				title="Hi I'm Hannah!"
-				description={'I am a visual artist based in Los Angeles, recent graduate from Biola University for a BFA in Design. From being interested in illustrating my own stories and creatures, to falling in love with visual design and language, I create art that strives to relate to other people. I specialize in art and spirituality, studying how the two are congruent with each other in order to better understand the humans we are designed to be.\n\n[Resume](https://res.cloudinary.com/josephdangerstewart/image/upload/v1589003199/hannah-creates-art/hannahresume.pdf)'}
+				description={aboutContent}
 				color="accent1"
 				imageUrl="https://res.cloudinary.com/josephdangerstewart/image/upload/v1588995401/hannah-creates-art/Profile_Picture.jpg"
 				fillImage
 			/>
-			<Container>
-				<BodyText>
-					<p>CONTACT INFO</p>
-					<p>
-						<A href="tel:1-949-395-4868" target="_blank" rel="noopener noreferrer nofollow">949.395.4868</A>
-						<br/>
-						<A href="mailto:hannah.mclaughlin.art@gmail.com" target="_blank" rel="noopener noreferrer nofollow">hannah.mclaughlin.art@gmail.com</A>
-						<br/>
-					</p>
-				</BodyText>
-			</Container>
 		</Page>
 	);
 }
